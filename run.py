66194b5dd6b375a7f7866d8c04816b539cdae520
phoenix <tohaukrainskih@gmail.com>
2016-02-05 18:54:42 +0700

    Сделал выход из окна

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import pygame
 import sys
 
-a = 0
+pygame.init()
+pygame.display.set_mode((800, 600))
+screen = pygame.display.get_surface()
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            sys.exit()
