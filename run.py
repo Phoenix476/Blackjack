318aec0f22c016a62ace1ad02d4d73299e9ebd98
Phoenix476 <tohaukrainskih@gmail.com>
2016-02-09 09:48:32 +0400

    Добавил кнопки

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+from Text import Text
 
 
 def load_image(name, alpha_channel):
@@ -20,11 +21,19 @@ pygame.font.init()
 pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Blackjack")
 screen = pygame.display.get_surface()
+# Кнопки
+hit_me = load_image('button-9096.png', 1)
+enough = load_image('button-9267.png', 1)
 # Карты
-image_card_1 = load_image('imgserver (1).png', 1)
+# image_card_1 = load_image('imgserver (1).png', 1)
+
+dealer_text = Text(14, 'Dealer', (400, 50))
+player_text = Text(14, 'Player', (400, 550))
+player_score = Text(12, "8/21", (220, 350))
+dealer_score = Text(12, '5/21', (220, 150))
 # Шрифты
-fontTextName = pygame.font.Font('freesansbold.ttf', 14)   # Шрифт имени игрока и дилера
-fontTextScore = pygame.font.Font('freesansbold.ttf', 12)  # Шрифт очков
+# fontTextName = pygame.font.Font('freesansbold.ttf', 14)   # Шрифт имени игрока и дилера
+# fontTextScore = pygame.font.Font('freesansbold.ttf', 12)  # Шрифт очков
 # -----------------------
 
 while True:
@@ -33,34 +42,37 @@ while True:
             sys.exit()
         if e.type == pygame.KEYDOWN:  # Событие "Клавиша нажата"
             print('Key Down')
-    # Дилер
-    textSurfaceDealer = fontTextName.render('Dealer', True, (0, 0, 0), (1, 50, 32))
-    textRectDealer = textSurfaceDealer.get_rect()
-    textRectDealer.center = (400, 50)
-    # ------------------------
-    # Имя игрока
-    textSurfacePlayer = fontTextName.render('Player', True, (0, 0, 0), (1, 50, 32))
-    textRectPlayer = textSurfacePlayer.get_rect()
-    textRectPlayer.center = (400, 550)
-    # -------------------------
-    # Очки Дилера
-    textSurfaceScoreDealer = fontTextScore.render('5/21', True, (0, 0, 0), (1, 50, 32))
-    textRectScoreDealer = textSurfaceScoreDealer.get_rect()
-    textRectScoreDealer.center = (220, 150)
-    # -------------------------
-    # Очки Игрока
-    textSurfaceScorePlayer = fontTextScore.render('8/21', True, (0, 0, 0), (1, 50, 32))
-    textRectScorePlayer = textSurfaceScoreDealer.get_rect()
-    textRectScorePlayer.center = (220, 350)
+    # # Дилер
+    # textSurfaceDealer = fontTextName.render('Dealer', True, (0, 0, 0), (1, 50, 32))
+    # textRectDealer = textSurfaceDealer.get_rect()
+    # textRectDealer.center = (400, 50)
+    # # ------------------------
+    # # Имя игрока
+    # textSurfacePlayer = fontTextName.render('Player', True, (0, 0, 0), (1, 50, 32))
+    # textRectPlayer = textSurfacePlayer.get_rect()
+    # textRectPlayer.center = (400, 550)
+    # # -------------------------
+    # # Очки Дилера
+    # textSurfaceScoreDealer = fontTextScore.render('5/21', True, (0, 0, 0), (1, 50, 32))
+    # textRectScoreDealer = textSurfaceScoreDealer.get_rect()
+    # textRectScoreDealer.center = (220, 150)
+    # # -------------------------
+    # # Очки Игрока
+    # textSurfaceScorePlayer = fontTextScore.render('8/21', True, (0, 0, 0), (1, 50, 32))
+    # textRectScorePlayer = textSurfaceScoreDealer.get_rect()
+    # textRectScorePlayer.center = (220, 350)
 
     screen.fill((1, 50, 32))
     # Выводит текст на экран
-    screen.blit(textSurfaceScoreDealer, textRectScoreDealer)
-    screen.blit(textSurfaceScorePlayer, textRectScorePlayer)
-    screen.blit(textSurfaceDealer, textRectDealer)
-    screen.blit(textSurfacePlayer, textRectPlayer)
+    screen.blit(dealer_text.get_surface, dealer_text.get_coords)
+    screen.blit(player_text.get_surface, player_text.get_coords)
+    screen.blit(dealer_score.get_surface, dealer_score.get_coords)
+    screen.blit(player_score.get_surface, player_score.get_coords)
 
     # Выводит карты на экран
-    screen.blit(image_card_1, (250, 150))
+    # screen.blit(image_card_1, (250, 150))
+
+    screen.blit(hit_me, (650, 400))
+    screen.blit(enough, (650, 330))
 
     pygame.display.flip()
